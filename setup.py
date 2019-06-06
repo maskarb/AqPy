546209d89e2367f2bb3d745363109274a18f5d7d
Michael Skarbek <maskarb@gmail.com>
2019-06-06 11:18:46 -0400

    create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='AqPy',
+    version='0.1',
+    packages=['aqpy',],
+    license='MIT license',
+    long_description=open('README.txt').read(),
+)
